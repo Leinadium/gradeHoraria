9564b75a37b0dd571731d6d64859dd007e1f033b
Leinadium <daniel.sch.guima@gmail.com>
2021-01-29 11:58:40 -0300

    bug fix

--- run_gunicorn.py
+++ run_gunicorn.py
@@ -1,12 +1,6 @@
-# run.py
-
-import os
-
 from app import create_app
 
-config_name = os.getenv('FLASK_CONFIG')
-app = create_app(config_name)
-
+app = create_app('production')
 
 if __name__ == '__main__':
     app.run()
